1ccdadc19e32a67b5b37a3e3c50aade7c7a8b86c
mja00 <teamnirvana2@gmail.com>
2024-09-11 18:26:18 -0400

    feat: fal.ai for image generation

--- src/commands/chat/generate-image-command.ts
+++ src/commands/chat/generate-image-command.ts
@@ -1,10 +1,35 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import * as fal from '@fal-ai/serverless-client';
 import { ChatInputCommandInteraction, PermissionsString } from 'discord.js';
+import { createRequire } from 'node:module';
 
 import { Language } from '../../models/enum-helpers/index.js';
 import { EventData } from '../../models/internal-models.js';
-import { Lang, OpenAIService } from '../../services/index.js';
+import { Lang, Logger, OpenAIService } from '../../services/index.js';
 import { InteractionUtils } from '../../utils/index.js';
 import { Command, CommandDeferType } from '../index.js';
+
+const require = createRequire(import.meta.url);
+let Config = require('../../../config/config.json');
+
+fal.config({
+    credentials: Config.fal.apiKey,
+});
+
+type FalResponse = {
+    images: Array<{
+        url: string;
+        width: number;
+        height: number;
+        content_type: string;
+    }>;
+    timings: {
+        inference: number;
+    };
+    seed: number;
+    has_nsfw_concepts: Array<boolean>;
+    prompt: string;
+};
 
 export class GenerateImageCommand implements Command {
     public names = [Lang.getRef('chatCommands.generateImage', Language.Default)];
@@ -16,15 +41,32 @@ export class GenerateImageCommand implements Command {
             prompt: intr.options.getString(Lang.getRef('arguments.prompt', Language.Default)),
         };
 
-        // Generate an image using the prompt
-        const openai = OpenAIService.getInstance();
-        // If there's an error here, it's most likely due to OpenAI being done with us
         try {
-            const image = await openai.generateImage(args.prompt);
+            const results: FalResponse = await fal.subscribe('fal-ai/flux/schnell', {
+                input: {
+                    prompt: args.prompt,
+                    image_size: 'landscape_4_3',
+                    num_images: 1,
+                },
+                logs: true,
+                onQueueUpdate: update => {
+                    if (update.status === 'IN_PROGRESS') {
+                        update.logs
+                            .map(log => log.message)
+                            .forEach(message => Logger.info(message));
+                    } else {
+                        Logger.info(update.status);
+                    }
+                },
+            });
 
-            await InteractionUtils.send(intr, `**${args.prompt}**\n${image}`);
+            const imageUrl = results.images[0].url;
+
+            // Just reply to the interaction
+            await InteractionUtils.send(intr, `**${args.prompt}**\n${imageUrl}`);
         } catch (error) {
-            await InteractionUtils.send(intr, `${error}`);
+            await InteractionUtils.send(intr, 'Something went wrong!');
+            Logger.error(error);
         }
     }
 }
